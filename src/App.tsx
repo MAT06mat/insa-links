690224c04c24adfbb0c94d8e80b1db72a4ad58f1
MAT06mat Desktop <mat06mat22@gmail.com>
2025-09-13 18:37:24 +0200

    feat: initialize Fastlinks project with React, Vite, and TypeScript
    - Added package.json with dependencies for React, Vite, and TypeScript.
    - Included SVG icons for branding.
    - Created main application structure with App component and styles.
    - Set up global styles in index.css and App.scss.
    - Configured TypeScript with appropriate settings in tsconfig files.
    - Established Vite configuration for building and serving the application.

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.scss";
+
+function App() {
+    return (
+        <>
+            <img src="/icon.svg" alt="Falstlink icon" />
+            <h1>Fastlinks</h1>
+            <div>
+                <ul>
+                    <li>A</li>
+                    <li>A</li>
+                    <li>A</li>
+                    <li>A</li>
+                </ul>
+            </div>
+        </>
+    );
+}
+
+export default App;
